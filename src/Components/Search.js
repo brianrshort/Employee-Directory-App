ebf492177cdc9fd6af6bc467a8705563f80b206b
Brian Short <brian.ralph.short@gmail.com>
2021-02-03 09:05:38 -0500

    Add searchbar for names search, handleChange function

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,11 +1,27 @@
-import React, { Component } from "react";
+import React from 'react';
 
-class Search extends Component {
-    render() {
-        return (
-            <div>Hey here's the search</div>
-        )
-    }
+export function Search(props) {
+
+    const handleChange = (e) => {
+        const terms = e.target.value;
+            const newArray = props.employees.filter(emp => {
+                if (emp.name.first.includes(terms) || emp.name.last.includes(terms)) {
+                   return emp;
+                }
+            }) ;
+            props.setEmployees(newArray);
+        }
+    
+    
+    return (
+        
+        <div className="input-group input-group-lg ml-5 mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-lg">Search by name</span>
+                </div>
+                <input type="text" class="form-control" aria-label="Large" 
+                    aria-describedby="inputGroup-sizing-sm" onChange={handleChange} />
+        </div>
+            
+    )
 }
-
-export default Search;
